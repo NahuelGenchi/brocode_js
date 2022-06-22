7863ddabf415bab4593ab4001dc89df4d1e6a853
Nahuel Genchi <nagen01@gmail.com>
2022-06-21 23:29:20 -0300

    30 number guessing game v2

--- 30 number guessing game/js/script.js
+++ 30 number guessing game/js/script.js
@@ -1,24 +1,32 @@
 const GUESSFIELD = document.querySelector("#guessField"),
-SUBMITBUTTON = document.querySelector("#submitButton");
+SUBMITBUTTON = document.querySelector("#submitButton"),
+RESULTTEXT = document.querySelector("#resultText"),
+GUESSESTEXT = document.querySelector("#guessesText");
+
+GUESSFIELD.focus();
 
 const ANSWER = Math.floor(Math.random() * 10 + 1);
 console.log(ANSWER);
 
 let guesses = 0;
 
-SUBMITBUTTON.addEventListener("click", submit);
+SUBMITBUTTON.addEventListener("click", findNumber);
 
-function submit() {
+function findNumber() {
     let guess = Number(GUESSFIELD.value);
     guesses++;
-    if (guess == ANSWER) {
-        alert(`${ANSWER} is the number. It took you ${guesses} guesses.`);
-        console.log(`${ANSWER} is the number. It took you ${guesses} guesses.`);
+    console.log(`Guesses: ${guesses}`);
+    if (guess == "") {
+        RESULTTEXT.textContent = `Type a valid number!`;
+        guesses--;
+    } else if (guess == ANSWER) {
+        RESULTTEXT.textContent = `${ANSWER} is the number. It took you ${guesses} guesses.`;
+        GUESSFIELD.disabled = true;
+        SUBMITBUTTON.disabled = true;
     } else if (guess > ANSWER) {
-        alert("Too large!");
-        console.log("Too large!");
+        RESULTTEXT.textContent = "Too large!";
     } else {
-        alert("Too small!");
-        console.log("Too small");
+        RESULTTEXT.textContent = "Too small!";
     };
+    GUESSESTEXT.textContent = `Guesses: ${guesses}`;
 };
